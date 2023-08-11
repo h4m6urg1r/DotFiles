04de094342f619a18d013d4d31608566969633ac
the-noob-coder69 <shantnuprofid@gmail.com>
2023-08-12 01:27:16 +0530

    Can't get wezterm config to work without this here

--- users/hamburgir/packages.d/gui/terminalEmu.nix
+++ users/hamburgir/packages.d/gui/terminalEmu.nix
@@ -5,6 +5,36 @@
         };
         wezterm = {
             enable = true;
+            extraConfig = ''
+                local wezterm = require "wezterm"
+                local config = {}
+            if wezterm.config_builder then
+                config = wezterm.config_builder()
+                    end
+                    config.font_size = 12.7
+                    config.font = wezterm.font("Mononoki Nerd Font Mono")
+                    --[[
+                    {
+                        weight = "Regular",
+                               stretch = "Normal",
+                               style = "Normal"
+                    }
+            --]]
+                config.color_scheme = 'Catppuccin Mocha'
+                config.window_background_gradient = {
+                    orientation = {
+                        Linear = {
+                            angle = 90
+                        }
+                    },
+                    colors = {
+                        '#0f0c29',
+                        '#302b63',
+                        '#24243e',
+                    },
+                }
+            return config
+        '';
         };
     };
 }
