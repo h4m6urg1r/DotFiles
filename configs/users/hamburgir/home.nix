7020be05442ba0473b99c6ccccc11563e097c960
the-noob-coder69 <shantnuprofid@gmail.com>
2023-08-16 17:10:15 +0530

    Removed redundant overlay

--- configs/users/hamburgir/home.nix
+++ configs/users/hamburgir/home.nix
@@ -1,15 +1,4 @@
 { pkgs, ... }: {
-    #nixpkgs.overlays = [
-    #    ( self: super: {
-    #            discord = super.discord.overrideAttrs(
-    #                old: {
-    #                    nativeBuildInputs = ( old.nativeBuildInputs or []) ++ [ self.makeWrapper ];
-    #                    postFixup = (old.postFixup or "") + '' wrapProgram $out/bin/discord --add-flags "--enable-features=UseOzonePlatform" --add-flags "--ozone-platform=wayland" '';
-    #                }
-    #            );
-    #        }
-    #    )
-    #];
     imports = [
         ./files.nix
         ./gui.nix
