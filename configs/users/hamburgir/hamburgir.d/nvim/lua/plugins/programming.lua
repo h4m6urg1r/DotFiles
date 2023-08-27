6807195537f969608f768eaff3f1d10b436f8615
the-noob-coder69 <shantnuprofid@gmail.com>
2023-08-27 23:24:06 +0530

    Added nvim-surround and a helper

--- configs/users/hamburgir/hamburgir.d/nvim/lua/plugins/programming.lua
+++ configs/users/hamburgir/hamburgir.d/nvim/lua/plugins/programming.lua
@@ -113,6 +113,29 @@ return {
         end
     },
     {
+        'kylechui/nvim-surround',
+        -- version = "", -- Use for stability; omit to use `main` branch for the latest features
+        event = 'VeryLazy',
+        config = function()
+            require('nvim-surround').setup({
+                -- Configuration here, or leave empty to use defaults
+            })
+        end
+    },
+    {
+        'roobert/surround-ui.nvim',
+        event = 'VeryLazy',
+        dependencies = {
+            'kylechui/nvim-surround',
+            'folke/which-key.nvim',
+        },
+        config = function()
+            require('surround-ui').setup({
+            root_key = 'S'
+            })
+        end,
+    },
+    {
         'onsails/diaglist.nvim',
         event = 'BufEnter'
     },
