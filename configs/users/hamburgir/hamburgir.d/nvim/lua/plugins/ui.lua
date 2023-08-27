e9eb57979750f643a182d402015f3c289a784ddb
the-noob-coder69 <shantnuprofid@gmail.com>
2023-08-27 23:27:12 +0530

    added fzf for telescope and ui improvement tools

--- configs/users/hamburgir/hamburgir.d/nvim/lua/plugins/ui.lua
+++ configs/users/hamburgir/hamburgir.d/nvim/lua/plugins/ui.lua
@@ -21,6 +21,40 @@ return {
         end,
         dependencies = {
             'nvim-lua/plenary.nvim',
+            'nvim-telescope/telescope-fzf-native.nvim'
         },
     },
+    {
+        'nvim-telescope/telescope-fzf-native.nvim',
+        lazy = true,
+        build = 'make',
+        -- build = 'cmake -S. -Bbuild -DCMAKE_BUILD_TYPE=Release && cmake --build build --config Release && cmake --install build --prefix build',
+    },
+    {
+        'stevearc/dressing.nvim',
+        event = 'UIEnter',
+        opts = {
+            title_pos = 'center',
+            border = 'rounded',
+            wrap = true,
+            mappings = {
+                n = {
+                    ["<Esc>"] = "Close",
+                    ["<CR>"] = "Confirm",
+                },
+                i = {
+                    ["<C-c>"] = "Close",
+                    ["<CR>"] = "Confirm",
+                    ["<Up>"] = "HistoryPrev",
+                    ["<Down>"] = "HistoryNext",
+                    ['<c-u>'] = 'HistoryPrev',
+                    ['<c-d>'] = 'HistoryPrev',
+                },
+            },
+        },
+    },
+    {
+        'rcarriga/nvim-notify',
+        event = 'UIEnter',
+    }
 }
