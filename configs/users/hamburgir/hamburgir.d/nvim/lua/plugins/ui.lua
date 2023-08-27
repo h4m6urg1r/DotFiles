74e4110ff3c05d7630e678a0f5d9179d92b48312
the-noob-coder69 <shantnuprofid@gmail.com>
2023-08-28 00:13:34 +0530

    Added floating window for commandline etc.

--- configs/users/hamburgir/hamburgir.d/nvim/lua/plugins/ui.lua
+++ configs/users/hamburgir/hamburgir.d/nvim/lua/plugins/ui.lua
@@ -55,7 +55,24 @@ return {
     },
     {
         'rcarriga/nvim-notify',
-        event = 'UIEnter',
+        lazy = true,
+        -- event = 'UIEnter',
         config = true,
+    },
+        -- lazy.nvim
+    {
+    "folke/noice.nvim",
+    event = "VeryLazy",
+    config = function ()
+        helpers.config('noice')
+    end,
+    dependencies = {
+        -- if you lazy-load any plugin below, make sure to add proper `module="..."` entries
+        "MunifTanjim/nui.nvim",
+        -- OPTIONAL:
+        --   `nvim-notify` is only needed, if you want to use the notification view.
+        --   If not available, we use `mini` as the fallback
+        "rcarriga/nvim-notify",
+        }
     }
 }
