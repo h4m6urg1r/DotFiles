228e4cb8c8d25fcb2417d3831a0dd54d8967a2be
the-noob-coder69 <shantnuprofid@gmail.com>
2023-09-05 01:21:31 +0530

    Hyprland windowRules fixed(kind of)

--- configs/users/hamburgir/hamburgir.d/hypr/windowRules.nix
+++ configs/users/hamburgir/hamburgir.d/hypr/windowRules.nix
@@ -1,6 +1,10 @@
 { config, ... }: {
     wayland.windowManager.hyprland.extraConfig = ''
-        windowrulev2 = float, class:(firefox), title:^(Close Firefox)
-        windowrule = float, class:^(firefox)
+        windowrulev2 = move 77% 76%, class:(firefox), title:(Picture-in-Picture)
+        windowrulev2 = size 22% 22%, class:(firefox), title:(Picture-in-Picture)
+        windowrulev2 = float, class:(firefox), title:(Picture-in-Picture)
+        windowrulev2 = nofullscreenrequest, class:(firefox), title:(Picture-in-Picture)
+        windowrulev2 = nomaximizerequest, class:(firefox), title:(Picture-in-Picture)
+
     '';
 }
