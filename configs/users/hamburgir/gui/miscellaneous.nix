003aa7b5a5ce45de52913eafe424a7875e97ae1e
Hamburgir <shantnuprofid@gmail.com>
2023-11-25 19:19:29 +0530

    Added ags and moved eww to right section of config

--- configs/users/hamburgir/gui/miscellaneous.nix
+++ configs/users/hamburgir/gui/miscellaneous.nix
@@ -1,4 +1,28 @@
-{ pkgs, ... }: {
+{ inputs, pkgs, ... }: {
+	imports = [
+		inputs.ags.homeManagerModules.default
+	];
+	nixpkgs.overlays = [
+		inputs.eww.overlays.default
+		inputs.rust-overlay.overlays.default
+	];
+
+	programs = {
+		ags = {
+			enable = true;
+			
+			# null or path, leave as null if you don't want hm to manage the config
+			configDir =	./ags;
+
+			# packages to add to gjs's runtime
+			extraPackages = [ pkgs.libsoup_3 ];
+		};
+		# eww = {
+		# 	enable = true;
+		# 	configDir = ./eww;
+		# 	package = pkgs.eww-wayland;
+		# };
+	};
 	home.packages = with pkgs; [
 		helvum
 		swww
