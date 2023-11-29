11d5c68d29d284d8af548b5d824e1ca4a3ff7ca5
Hamburgir <shantnuprofid@gmail.com>
2023-11-29 21:56:24 +0530

    Rofi theme++

--- configs/users/hamburgir/packages.nix
+++ configs/users/hamburgir/packages.nix
@@ -1,4 +1,4 @@
-{ inputs, pkgs, ... }: {
+{ inputs, config, pkgs, ... }: {
     home.packages = with pkgs; [
         discord
         ranger
@@ -8,17 +8,15 @@
         zathura
         variety
         rnote
-        (nerdfonts.override{fonts=["Mononoki"];})
+        (nerdfonts.override{fonts=["Mononoki" "Iosevka"];})
     ];
 
     # imports = [
     #     #hyprland.homeManagerModules.default
     #     ./packages.d
     # ];
-    nixpkgs.overlays = [
-        inputs.eww.overlays.default
-        inputs.rust-overlay.overlays.default
-    ];
+    # nixpkgs.overlays = [
+    # ];
 
     programs = {
         aria2 = {
@@ -51,197 +49,194 @@
         command-not-found = {
             enable = true;
         };
-        eww = {
-            enable = true;
-            configDir = ./hamburgir.d/eww;
-            package = pkgs.eww-wayland;
-        };
         firefox = {
             enable = true;
         };
         #try havoc
         #try helix
         #htop
-        rofi = {
-            enable = true;
-            plugins = with pkgs; [
-                rofi-calc
-                rofi-emoji
-                rofi-pass
-                rofi-power-menu
-                rofi-file-browser
-            ];
-            configPath = "$HOME/.cache/rofi/config.rasi";
-            # theme = let
-            #     inherit (config.lib.formats.rasi) mkLiteral;
-            # in {
-            #     ""
-            # };
-# /*****----- Configuration -----*****/
-# configuration {
-#   modi:                       "drun";
-#     show-icons:                 true;
-#     display-drun:               "";
-#   drun-display-format:        "{name}";
-# }
-#
-# /*****----- Global Properties -----*****/
-# @import                          "shared/colors.rasi"
-# @import                          "shared/fonts.rasi"
-#
-# /*****----- Main Window -----*****/
-# window {
-#     transparency:                "real";
-#     location:                    center;
-#     anchor:                      center;
-#     fullscreen:                  true;
-#     width:                       1366px;
-#     height:                      768px;
-#     x-offset:                    0px;
-#     y-offset:                    0px;
-#
-#     enabled:                     true;
-#     margin:                      0px;
-#     padding:                     0px;
-#     border:                      0px solid;
-#     border-radius:               0px;
-#     border-color:                @selected;
-#     background-color:            black / 10%;
-#     cursor:                      "default";
-# }
-#
-# /*****----- Main Box -----*****/
-# mainbox {
-#     enabled:                     true;
-#     spacing:                     100px;
-#     margin:                      0px;
-#     padding:                     100px 225px;
-#     border:                      0px solid;
-#     border-radius:               0px 0px 0px 0px;
-#     border-color:                @selected;
-#     background-color:            transparent;
-#     children:                    [ "inputbar", "listview" ];
-# }
-#
-# /*****----- Inputbar -----*****/
-# inputbar {
-#     enabled:                     true;
-#     spacing:                     10px;
-#     margin:                      0% 28%;
-#     padding:                     10px;
-#     border:                      1px solid;
-#     border-radius:               6px;
-#     border-color:                white / 25%;
-#     background-color:            white / 5%;
-#     text-color:                  @foreground;
-#     children:                    [ "prompt", "entry" ];
-# }
-#
-# prompt {
-#     enabled:                     true;
-#     background-color:            transparent;
-#     text-color:                  inherit;
-# }
-# textbox-prompt-colon {
-#     enabled:                     true;
-#     expand:                      false;
-#     str:                         "::";
-#     background-color:            transparent;
-#     text-color:                  inherit;
-# }
-# entry {
-#     enabled:                     true;
-#     background-color:            transparent;
-#     text-color:                  inherit;
-#     cursor:                      text;
-#     placeholder:                 "Search";
-#     placeholder-color:           inherit;
-# }
-#
-# /*****----- Listview -----*****/
-# listview {
-#     enabled:                     true;
-#     columns:                     7;
-#     lines:                       4;
-#     cycle:                       true;
-#     dynamic:                     true;
-#     scrollbar:                   false;
-#     layout:                      vertical;
-#     reverse:                     false;
-#     fixed-height:                true;
-#     fixed-columns:               true;
-#     
-#     spacing:                     0px;
-#     margin:                      0px;
-#     padding:                     0px;
-#     border:                      0px solid;
-#     border-radius:               0px;
-#     border-color:                @selected;
-#     background-color:            transparent;
-#     text-color:                  @foreground;
-#     cursor:                      "default";
-# }
-# scrollbar {
-#     handle-width:                5px ;
-#     handle-color:                @selected;
-#     border-radius:               0px;
-#     background-color:            @background-alt;
-# }
-#
-# /*****----- Elements -----*****/
-# element {
-#     enabled:                     true;
-#     spacing:                     15px;
-#     margin:                      0px;
-#     padding:                     35px 10px;
-#     border:                      0px solid;
-#     border-radius:               15px;
-#     border-color:                @selected;
-#     background-color:            transparent;
-#     text-color:                  @foreground;
-#     orientation:                 vertical;
-#     cursor:                      pointer;
-# }
-# element normal.normal {
-#     background-color:            transparent;
-#     text-color:                  @foreground;
-# }
-# element selected.normal {
-#     background-color:            white / 5%;
-#     text-color:                  @foreground;
-# }
-# element-icon {
-#     background-color:            transparent;
-#     text-color:                  inherit;
-#     size:                        72px;
-#     cursor:                      inherit;
-# }
-# element-text {
-#     background-color:            transparent;
-#     text-color:                  inherit;
-#     highlight:                   inherit;
-#     cursor:                      inherit;
-#     vertical-align:              0.5;
-#     horizontal-align:            0.5;
-# }
-#
-# /*****----- Message -----*****/
-# error-message {
-#     padding:                     100px;
-#     border:                      0px solid;
-#     border-radius:               0px;
-#     border-color:                @selected;
-#     background-color:            black / 10%;
-#     text-color:                  @foreground;
-# }
-# textbox {
-#     background-color:            transparent;
-#     text-color:                  @foreground;
-#     vertical-align:              0.5;
-#     horizontal-align:            0.0;
-#     highlight:                   none;
-# }
-        };
+		rofi = {
+			enable = true;
+			plugins = with pkgs; [
+				rofi-calc
+				rofi-emoji
+				rofi-pass
+				rofi-power-menu
+				rofi-file-browser
+			];
+			# configPath = "$HOME/.cache/rofi/config.rasi";
+			extraConfig = {
+				modi = "drun,emoji,run";
+				show-icons = true;
+				display-drun = "";
+				drun-display-format = "{name}";
+			};
+			theme = let
+				inherit (config.lib.formats.rasi) mkLiteral;
+			in {
+				"*" = {
+					font = "Iosevka Nerd Font 14";
+					background = "#1E1D2FFF";
+					background-alt = "#282839FF";
+					foreground = "#D9E0EEFF";
+					selected = "#7AA2F7FF";
+					active = "#ABE9B3FF";
+					urgent = "#F28FADFF";
+				};
+
+				window = {
+					transparency = "real";
+					location = mkLiteral "center";
+					anchor = mkLiteral "center";
+					fullscreen = true;
+					width = mkLiteral "1366px";
+					height = mkLiteral "768px";
+					x-offset = mkLiteral "0px";
+					y-offset = mkLiteral "0px";
+
+					enabled = true;
+					margin = mkLiteral "0px";
+					padding = mkLiteral "0px";
+					border = mkLiteral "0px solid";
+					border-radius = mkLiteral "0px";
+					border-color = mkLiteral "@selected";
+					background-color = mkLiteral "black / 10%";
+					cursor = "default";
+				};
+
+				# /*****----- Main Box -----*****/
+				mainbox = {
+					enabled = true;
+					spacing = mkLiteral "100px";
+					margin = mkLiteral "0px";
+					padding = mkLiteral "100px 225px";
+					border = mkLiteral "0px solid";
+					border-radius = mkLiteral "0px 0px 0px 0px";
+					border-color = mkLiteral "@selected";
+					background-color = mkLiteral "transparent";
+					children = [ "inputbar" "listview" ];
+				};
+
+				# /*****----- Inputbar -----*****/
+				inputbar = {
+					enabled = true;
+					spacing = mkLiteral "10px";
+					margin = mkLiteral "0% 28%";
+					padding = mkLiteral "10px";
+					border = mkLiteral "1px solid";
+					border-radius = mkLiteral "6px";
+					border-color = mkLiteral "white / 25%";
+					background-color = mkLiteral "white / 5%";
+					text-color = mkLiteral "@foreground";
+					children = [ "prompt" "entry" ];
+				};
+
+				prompt = {
+					enabled = true;
+					background-color = mkLiteral "transparent";
+					text-color = mkLiteral "inherit";
+				};
+				textbox-prompt-colon = {
+					enabled = true;
+					expand = false;
+					str = "::";
+					background-color = mkLiteral "transparent";
+					text-color = mkLiteral "inherit";
+				};
+				entry = {
+					enabled = true;
+					background-color = mkLiteral "transparent";
+					text-color = mkLiteral "inherit";
+					cursor = mkLiteral "text";
+					placeholder = "Search";
+					placeholder-color = mkLiteral "inherit";
+				};
+
+				# /*****----- Listview -----*****/
+				listview = {
+					enabled = true;
+					columns = 7;
+					lines = 4;
+					cycle = true;
+					dynamic = true;
+					scrollbar = false;
+					layout = mkLiteral "vertical";
+					reverse = false;
+					fixed-height = true;
+					fixed-columns = true;
+					
+					spacing = mkLiteral "0px";
+					margin = mkLiteral "0px";
+					padding = mkLiteral "0px";
+					border = mkLiteral "0px solid";
+					border-radius = mkLiteral "0px";
+					border-color = mkLiteral "@selected";
+					background-color = mkLiteral "transparent";
+					text-color = mkLiteral "@foreground";
+					cursor = "default";
+				};
+				scrollbar = {
+					handle-width = mkLiteral "5px ";
+					handle-color = mkLiteral "@selected";
+					border-radius = mkLiteral "0px";
+					background-color = mkLiteral "@background-alt";
+				};
+
+				# /*****----- Elements -----*****/
+				element = {
+					enabled = mkLiteral "true";
+					spacing = mkLiteral "15px";
+					margin = mkLiteral "0px";
+					padding = mkLiteral "35px 10px";
+					border = mkLiteral "0px solid";
+					border-radius = mkLiteral "15px";
+					border-color = mkLiteral "@selected";
+					background-color = mkLiteral "transparent";
+					text-color = mkLiteral "@foreground";
+					orientation = mkLiteral "vertical";
+					cursor = mkLiteral "pointer";
+				};
+				"element normal.normal" = {
+					background-color = mkLiteral "transparent";
+					text-color = mkLiteral "@foreground";
+				};
+				"element selected.normal" = {
+					background-color = mkLiteral "white / 5%";
+					text-color = mkLiteral "@foreground";
+				};
+				element-icon = {
+					background-color = mkLiteral "transparent";
+					text-color = mkLiteral "inherit";
+					size = mkLiteral "72px";
+					cursor = mkLiteral "inherit";
+				};
+				element-text = {
+					background-color = mkLiteral "transparent";
+					text-color = mkLiteral "@foreground";
+					highlight = mkLiteral "inherit";
+					cursor = mkLiteral "inherit";
+					vertical-align = mkLiteral "0.5";
+					horizontal-align = mkLiteral "0.5";
+				};
+
+				# /*****----- Message -----*****/
+				error-message = {
+					padding = mkLiteral "100px";
+					border = mkLiteral "0px solid";
+					border-radius = mkLiteral "0px";
+					border-color = mkLiteral "@selected";
+					background-color = mkLiteral "black / 10%";
+					text-color = mkLiteral "@foreground";
+				};
+				textbox = {
+					background-color = mkLiteral "transparent";
+					text-color = mkLiteral "@foreground";
+					vertical-align = mkLiteral "0.5";
+					horizontal-align = mkLiteral "0.0";
+					highlight = mkLiteral "none";
+				};
+			};
+		};
         #wofi = {
         #    enable = true;
         #};
