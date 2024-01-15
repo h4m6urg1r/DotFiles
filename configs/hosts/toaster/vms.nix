7e88250eedc9d4ced0e7843178f2f481e7922825
Hamburgir <shantnuprofid@gmail.com>
2024-01-15 15:53:15 +0530

    Enabled virt-manager

--- configs/hosts/toaster/vms.nix
+++ configs/hosts/toaster/vms.nix
@@ -1,26 +1,30 @@
 { config, ... }: {
-    # virtualisation.resolution = { x = 1024; y = 768; };
-    virtualisation = {
-        waydroid.enable = false;
-        # lxd.enable = true;
-        # docker = {
-        #  enable = false;
-        #  rootless = {
-        #    enable = true;
-        #    setSocketVariable = true;
-        #  };
-        # };
-        podman = {
-        enable = true;
-                # Create a `docker` alias for podman, to use it as a drop-in replacement
-        dockerCompat = true;
+	# virtualisation.resolution = { x = 1024; y = 768; };
+	virtualisation = {
+		waydroid.enable = false;
+		# lxd.enable = true;
+		# docker = {
+		#  enable = false;
+		#  rootless = {
+		#    enable = true;
+		#    setSocketVariable = true;
+		#  };
+		# };
+		podman = {
+			enable = true;
+			# Create a `docker` alias for podman, to use it as a drop-in replacement
+			dockerCompat = true;
 
-        # Required for containers under podman-compose to be able to talk to each other.
-        defaultNetwork.settings.dnsname.enable = true;
-        # For Nixos version > 22.11
-        #defaultNetwork.settings = {
-        #  dns_enabled = true;
-        #};
-        };
-    };
+			# Required for containers under podman-compose to be able to talk to each other.
+			defaultNetwork.settings.dnsname.enable = true;
+			# For Nixos version > 22.11
+			#defaultNetwork.settings = {
+			#  dns_enabled = true;
+			#};
+		};
+
+		libvirtd.enable=true;
+	};
+	# virtualisation.libvirtd.enable = true;
+	programs.virt-manager.enable = true;
 }
