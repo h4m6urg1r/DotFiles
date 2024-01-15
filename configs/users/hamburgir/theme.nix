3a183671f3dee5de11ff681f45c94a3830c13172
Hamburgir <shantnuprofid@gmail.com>
2024-01-15 15:47:50 +0530

    Qt theming!

--- configs/users/hamburgir/theme.nix
+++ configs/users/hamburgir/theme.nix
@@ -1,26 +1,35 @@
 { pkgs, ... }: {
-    gtk = {
-        enable = true;
-        font = {
-            name = "DejaVu Sans";
-            size = 11;
-        };
-        iconTheme = {
-            package = pkgs.papirus-icon-theme;
-            name = "Papirus-Dark";
-        };
-        theme = {
-            package = pkgs.gnome.gnome-themes-extra;
-            name = "Adwaita-dark";
-        };
-    };
+	gtk = {
+		enable = true;
+		font = {
+			name = "DejaVu Sans";
+			size = 11;
+		};
+		iconTheme = {
+			package = pkgs.papirus-icon-theme;
+			name = "Papirus-Dark";
+		};
+		theme = {
+			package = pkgs.gnome.gnome-themes-extra;
+			name = "Adwaita-dark";
+		};
+	};
 
-    # home.pointerCursor = {
-    #   package = pkgs.vanilla-dmz;
-    #   gtk.enable = true;
-    #   name = "Vanilla-DMZ";
-    #   size = 24;
-    #   x11.enable = true;
-    # };
-    fonts.fontconfig.enable = false;
+	qt = {
+		enable = true;
+		# platformTheme = "gtk3";
+		style = {
+			package = pkgs.adwaita-qt;
+			name = "adwaita-dark";
+		};
+	};
+
+	# home.pointerCursor = {
+	#   package = pkgs.vanilla-dmz;
+	#   gtk.enable = true;
+	#   name = "Vanilla-DMZ";
+	#   size = 24;
+	#   x11.enable = true;
+	# };
+	fonts.fontconfig.enable = false;
 }
