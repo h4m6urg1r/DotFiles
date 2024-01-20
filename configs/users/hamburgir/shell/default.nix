ff8317621f24172060670f5e1c579554c9d306dc
Hamburgir <shantnuprofid@gmail.com>
2024-01-20 17:49:17 +0530

    AGS python pam authentication module installed

--- configs/users/hamburgir/shell/default.nix
+++ configs/users/hamburgir/shell/default.nix
@@ -1,10 +1,15 @@
 { pkgs, ... }: {
 	imports = [
+		# ./bin
 		./editor.nix
 		./filemanager.nix
+		./notifications.nix
 		./media.nix
 		./shell.nix
 		./utils.nix
 		./vcs.nix
+	];
+	home.packages = with pkgs; [
+		(python3.withPackages (ps: with ps;[ python-pam ]))
 	];
 }
