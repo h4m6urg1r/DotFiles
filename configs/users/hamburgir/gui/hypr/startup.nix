383fc1af132a0e707da36f817285131876aa1978
Hamburgir <shantnuprofid@gmail.com>
2024-01-21 16:25:12 +0530

    Changed hyprland startup apps

--- configs/users/hamburgir/gui/hypr/startup.nix
+++ configs/users/hamburgir/gui/hypr/startup.nix
@@ -1,11 +1,11 @@
 { config, ... }: {
 	wayland.windowManager.hyprland.settings = {
 		exec-once = [
-			# "eww open topbar-hyprland"
+			"eww open topbar-hyprland"
 			# "waybar"
-			"ags"
+			# "ags"
 			"swww init"
-			"nm-applet"
+			# "nm-applet"
 		];
 		exec = [
 			"swww query && swww img ${config.home.homeDirectory}/.config/background.jpg"
