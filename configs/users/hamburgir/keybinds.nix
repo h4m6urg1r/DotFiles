6e261f87caa1f1a631ae23eb90b410370a70a237
Hamburgir <shantnuprofid@gmail.com>
2024-01-24 02:29:15 +0530

    idk again

--- configs/users/hamburgir/keybinds.nix
+++ configs/users/hamburgir/keybinds.nix
@@ -1,30 +1,30 @@
 { inputs, pkgs, ... }: {
 	imports = [
-		inputs.xremap.homeManagerModules.default
+		# inputs.xremap.homeManagerModules.default
 	];
 	home.packages = with pkgs; [
-		inputs.xremap
+		# inputs.xremap
 	];
-	services.xremap = {
-		withWlroots = true;
-		config = {
-			keymap = [
-				{
-					name = "Applications";
-					remap = {
-						super-o = {
-							remap = {
-								b = {
-									launch = ["firefox"];
-								};
-								s = {
-									launch = [ "pavucontrol" ];
-								};
-							};
-						};
-					};
-				}
-			];
-		};
-	};
+	# services.xremap = {
+	# 	withWlroots = true;
+	# 	config = {
+	# 		keymap = [
+	# 			{
+	# 				name = "Applications";
+	# 				remap = {
+	# 					super-o = {
+	# 						remap = {
+	# 							b = {
+	# 								launch = ["firefox"];
+	# 							};
+	# 							s = {
+	# 								launch = [ "pavucontrol" ];
+	# 							};
+	# 						};
+	# 					};
+	# 				};
+	# 			}
+	# 		];
+	# 	};
+	# };
 }
