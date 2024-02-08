a02ba96c98065aaa76e2f05a391927b22ea2e5ae
Hamburgir <shantnuprofid@gmail.com>
2024-02-09 03:30:41 +0530

    Using ags for bar and such

--- users/hamburgir/gui/hypr/startup.nix
+++ users/hamburgir/gui/hypr/startup.nix
@@ -1,13 +1,13 @@
 { config, pkgs, ... }: {
 	wallpaper.desktop = {
 		dir = "your_name";
-		file = "MitsuhaTakiFirstMeet.jpg";
+		file = "MitsuhaComet.jpg";
 	};
 	wayland.windowManager.hyprland.settings = {
 		exec-once = [
-			"eww open topbar-hyprland"
+			# "eww open topbar-hyprland"
 			# "waybar"
-			# "ags"
+			"ags"
 			# "nm-applet"
 			"${pkgs.wl-clipboard}/bin/wl-paste -t image --watch ${pkgs.cliphist}/bin/cliphist store"
 			"${pkgs.wl-clipboard}/bin/wl-paste -t text --watch ${pkgs.cliphist}/bin/cliphist store"
