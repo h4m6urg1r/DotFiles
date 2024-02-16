f31c7fa0ef1adf70e113b341ef54c2e9fca9b5e3
Hamburgir <shantnuprofid@gmail.com>
2024-02-16 20:08:24 +0530

    added custom vim packages

--- overlays/default.nix
+++ overlays/default.nix
@@ -1,7 +1,9 @@
 # This file defines overlays
 { inputs, ... }: {
 	# This one brings our custom packages from the 'pkgs' directory
-	additions = final: _prev: import ../pkgs { pkgs = final; };
+	additions = final: prev: import ../pkgs { pkgs = final; } // {
+		vimPlugins = prev.vimPlugins // final.callPackage ../pkgs/vim-plugins {  };
+	};
 
 	# This one contains whatever you want to overlay
 	# You can change versions, add patches, set compilation flags, anything really.
